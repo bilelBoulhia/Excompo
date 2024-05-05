fd6669f12441bdd4cfef822b5607418e829ed554
bilel0509 <billel.boulahia456@gmail.com>
2024-05-05 10:20:22 +0100

    sadsa

---
+++ src/Effect Hooks/useInsertApplicants.jsx
@@ -0,0 +1,35 @@
+﻿
+import axios from 'axios';
+
+
+import { useToast } from '@chakra-ui/react'
+
+
+
+const API_BASE_URL = 'https://localhost:7131/SubmitApplicant';
+
+export async function useInsertApplicants(data) {
+    try {
+        const formData = new FormData();
+        formData.append('Email', data.Email);
+        formData.append('Nom',data.Nom  );
+        formData.append('Prenom', data.Prenom );
+        formData.append('NumeroPhone', data.NumeroPhone );
+        formData.append('AssoEvent' , data.AssoEvent)
+
+
+        await axios.post(API_BASE_URL, formData);
+
+
+
+        return true
+    } catch (error) {
+        console.error(' error:', error.message);
+        if (error.response) {
+            console.log(' status:', error.response.status);
+            console.log('data:', error.response.data);
+        }
+
+        return false
+    }
+}
